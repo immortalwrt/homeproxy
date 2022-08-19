681f1f7180e7c7f53ca149cecfc5aeb971d8fd0e
Tianling Shen <cnsztl@gmail.com>
2022-08-20 06:09:39 +0800

    feat(status): add basic status page

---
+++ htdocs/luci-static/resources/view/homeproxy/status.js
@@ -0,0 +1,170 @@
+/* SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Copyright (C) 2022 ImmortalWrt.org
+ */
+
+'use strict';
+'require dom';
+'require form';
+'require fs';
+'require poll';
+'require rpc';
+'require ui';
+'require view';
+
+/* Thanks to luci-app-aria2 */
+var css = '				\
+#log_textarea {				\
+	padding: 10px;			\
+	text-align: left;		\
+}					\
+#log_textarea pre {			\
+	padding: .5rem;		\
+	word-break: break-all;		\
+	margin: 0;			\
+}					\
+.description {				\
+	background-color: #33ccff;	\
+}';
+
+var hp_dir = '/var/run/homeproxy';
+var hp_geoupdater = '/etc/homeproxy/scripts/update_geodata.sh';
+
+return view.extend({
+	load: function() {
+		return Promise.all([
+			fs.read(hp_dir + '/homeproxy.log', 'text') .then(function(res) {
+				return res.trim() || _('Log is clean.');
+			}).catch(function(err) {
+				var log;
+				if (err.toString().includes('NotFoundError'))
+					log = _('Log is not found.');
+				else
+					log = _('Unknown error: %s').format(err);
+				return log;
+			})
+		])
+	},
+
+	render: function(data) {
+		var m, s, o;
+
+		m = new form.Map('homeproxy');
+
+		s = m.section(form.NamedSection, 'config', 'homeproxy', _('Service status'));
+		s.anonymous = true;
+
+		o = s.option(form.DummyValue, '_geodata_version', _('GeoData version'));
+		o.cfgvalue = function() {
+			var _this = this;
+			var spanTemp = '<div style="margin-top:7px;margin-left:3px;">%s</div>';
+
+			return fs.exec(hp_geoupdater, [ 'get_version' ]).then(function(res) {
+				if (res.stdout.trim())
+					_this.default = String.format(spanTemp, res.stdout.trim());
+				else {
+					ui.addNotification(null, E('p', [ _('%s。').format(res) ]));
+					_this.default = String.format(spanTemp, String.format('<strong style="color:red">', _('unknown error')));
+				}
+
+				return null;
+			}).catch(function(err) {
+				ui.addNotification(null, E('p', [ _('未知错误：%s。').format(err) ]));
+				_this.default = String.format(spanTemp, 'red', _('未知错误'));
+
+				return null;
+			});
+		}
+		o.rawhtml = true;
+
+		o = s.option(form.Button, '_update_geodata', _('Update GeoData'));
+		o.inputstyle = 'action';
+		o.onclick = function() {
+			var _this = this;
+
+			return fs.exec(hp_geoupdater, [ 'update_version' ]).then(function (res) {
+					if (res.code === 0)
+						_this.description = _('Successfully updated');
+					else if (res.code === 1)
+						_this.description = _('Update failed');
+					else if (res.code === 2)
+						_this.description = _('Already in updating');
+					else if (res.code === 3)
+						_this.description = _('Already at the latest version');
+
+				return _this.map.reset();
+			}).catch(function (err) {
+				ui.addNotification(null, E('p', [ _('Unknown error: %s').format(err) ]));
+				_this.description = _('Update failed');
+				return _this.map.reset();
+			});
+		}
+
+		o = s.option(form.DummyValue, '_homeproxy_logview');
+		o.render = function() {
+			return E([
+				E('style', [ css ]),
+				E('div', {'class': 'cbi-map'}, [
+					E('h3', {'name': 'content'}, _('HomeProxy Log')),
+					E('div', {'class': 'cbi-section'}, [
+						E('div', { 'id': 'log_textarea' },
+							E('pre', { 'wrap': 'pre' }, [ data[0] ])
+						)
+					])
+				])
+			]);
+		}
+
+		o = s.option(form.DummyValue, '_sing-box_logview');
+		o.render = function() {
+			var log_textarea = E('div', { 'id': 'log_textarea' },
+				E('img', {
+					'src': L.resource(['icons/loading.gif']),
+					'alt': _('Loading'),
+					'style': 'vertical-align:middle'
+				}, _('Collecting data...'))
+			);
+
+			poll.add(L.bind(function() {
+				return fs.read(hp_dir + '/sing-box.log', 'text')
+				.then(function(res) {
+					var log = E('pre', { 'wrap': 'pre' }, [
+						res.trim() || _('Log is clean.')
+					]);
+
+					dom.content(log_textarea, log);
+				}).catch(function(err) {
+					if (err.toString().includes('NotFoundError'))
+						var log = E('pre', { 'wrap': 'pre' }, [
+							_('Log not found.')
+						]);
+					else
+						var log = E('pre', { 'wrap': 'pre' }, [
+							_('Unknown error: %s').format(err)
+						]);
+
+					dom.content(log_textarea, log);
+				});
+			}));
+
+			return E([
+				E('style', [ css ]),
+				E('div', {'class': 'cbi-map'}, [
+					E('h3', {'name': 'content'}, _('Sing-box Log')),
+					E('div', {'class': 'cbi-section'}, [
+						log_textarea,
+						E('div', {'style': 'text-align:right'},
+							E('small', {}, _('Refresh every %s seconds.').format(L.env.pollinterval))
+						)
+					])
+				])
+			]);
+		}
+
+		return m.render();
+	},
+
+	handleSaveApply: null,
+	handleSave: null,
+	handleReset: null
+});
---
+++ root/etc/homeproxy/scripts/update_geodata.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Copyright (C) 2022 ImmortalWrt.org
+
+NAME="homeproxy"
+
+GEODATA_DIR="/etc/$NAME/resources"
+GEOIP_REPO="1715173329/sing-geoip"
+GEOSITE_REPO="1715173329/sing-geosite"
+mkdir -p "$GEODATA_DIR"
+
+RUN_DIR="/var/run/$NAME"
+LOG_PATH="$RUN_DIR/$NAME.log"
+LOCK="$RUN_DIR/update_geodata.lock"
+mkdir -p "$RUN_DIR"
+
+log() {
+	echo -e "$(date "+%Y-%m-%d %H:%M:%S") [GeoData Updater] $*" >> "$LOG_PATH"
+}
+
+check_multi_task() {
+	if [ -e "$LOCK" ]; then
+		log "A task is already running."
+		exit 2
+	else
+		touch "$LOCK"
+	fi
+}
+
+check_update() {
+	local geotype="$1"
+	local georepo="$2"
+	local geotypeupper="$(to_upper "$geotype")"
+
+	local geodata_ver="$(curl --connect-timeout 5 -fsSL "https://api.github.com/repos/$georepo/releases/latest" | jsonfilter -e "@.tag_name")"
+	if [ -z "$geodata_ver" ]; then
+		log "[$geotypeupper] Failed to get the latest version, please retry later."
+		return 1
+	fi
+
+	local_geodata_ver="$(cat "$GEODATA_DIR/$geotype.db.ver" 2>"/dev/null" || echo "NOT FOUND")"
+	if [ "$local_geodata_ver" = "$geodata_ver" ]; then
+		log "[$geotypeupper] Current version: $geodata_ver."
+		log "[$geotypeupper] You're already at the latest version."
+		return 3
+	else
+		log "[$geotypeupper] Local version: $local_geodata_ver, latest version: $geodata_ver."
+	fi
+
+	local geodata_hash
+	if curl --connect-timeout 5 -fsSL "https://github.com/$georepo/releases/download/$geodata_ver/$geotype.db" -o "$RUN_DIR/$geotype.db" && \
+			geodata_hash="$(curl --connect-timeout 5 -fsSL "https://github.com/$georepo/releases/download/$geodata_ver/$geotype.db.sha256sum")"; then
+		[ -z "$geodata_hash" ] || geodata_hash="$(echo "$geodata_hash" | awk '{print $1}')"
+		if validate_sha256sum "$RUN_DIR/$geotype.db" "$geodata_hash"; then
+			mv -f "$RUN_DIR/$geotype.db" "$GEODATA_DIR/$geotype.db"
+			echo -e "$geodata_ver" > "$GEODATA_DIR/$geotype.db.ver"
+			log "[$geotypeupper] Successfully updated."
+			return 0
+		fi
+	fi
+
+	rm -f "$RUN_DIR/$geotype.db"
+	log "[$geotypeupper] Update failed."
+	return 1
+}
+
+to_upper() {
+	echo -e "$1" | tr "[a-z]" "[A-Z]"
+}
+
+validate_sha256sum() {
+	local file="$1"
+	local hash="$2"
+
+	if [ ! -e "$file" ] || [ -z "$hash" ]; then
+		return 1
+	fi
+
+	local filehash="$(sha256sum "$file" | awk '{print $1}')"
+	if [ -z "$filehash" ] || [ "$filehash" != "$hash" ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+case "$1" in
+"get_version")
+	for i in "geoip" "geosite"; do
+		if [ ! -s "$GEODATA_DIR/$i.db.ver" ]; then
+			info="${info:+$info<br/>}<strong style=\"color:red\">$(to_upper "$i"): NOT FOUND</strong>"
+		else
+			info="${info:+$info<br/>}<strong style=\"color:green\">$(to_upper "$i"): $(cat $GEODATA_DIR/$i.db.ver)</strong>"
+		fi
+	done
+	echo -e "$info"
+	;;
+"update_version")
+	check_multi_task
+
+	check_update "geoip" "$GEOIP_REPO"
+	ret1="$?"
+	check_update "geosite" "$GEOSITE_REPO"
+	ret2="$?"
+
+	rm -f "$LOCK"
+
+	if [ "$ret1" = "1" ] || [ "$ret2" = "1" ]; then
+		exit 1
+	elif [ "$ret1" = "0" ] || [ "$ret2" = "0" ]; then
+		exit 0
+	else
+		exit 3
+	fi
+	;;
+*)
+	echo -e "Usage: $0 get_version | update_version"
+	exit 1
+	;;
+esac
