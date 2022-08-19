7c1a17f66bc5c9935edfe41f31790b6dd934a358
Tianling Shen <cnsztl@gmail.com>
2022-08-19 10:48:47 +0800

    chore: reduce redudant code

--- htdocs/luci-static/resources/view/homeproxy/node.js
+++ htdocs/luci-static/resources/view/homeproxy/node.js
@@ -1196,49 +1196,27 @@ return view.extend({
 
 		o = s.option(form.ListValue, 'tls_min_version', _('Minimum TLS version'),
 			_('The minimum TLS version that is acceptable. Default to 1.0.'));
-		o.value('1.0');
-		o.value('1.1');
-		o.value('1.2');
-		o.value('1.3');
+		for (var i in hp.tls_versions)
+			o.value(hp.tls_versions[i])
 		o.default = '1.0';
 		o.depends({'type': 'http', 'tls': '1'});
 		o.depends({'type': 'trojan', 'tls': '1'});
 		o.depends({'type': 'vmess', 'tls': '1'});
-		o.rmempty = false;
 		o.modalonly = true;
 
 		o = s.option(form.ListValue, 'tls_max_version', _('Maximum TLS version'),
 			_('The maximum TLS version that is acceptable. Default to 1.3.'));
-		o.value('1.0');
-		o.value('1.1');
-		o.value('1.2');
-		o.value('1.3');
-		o.default = '1.3';
+		for (var i in hp.tls_versions)
+			o.value(hp.tls_versions[i])
 		o.depends({'type': 'http', 'tls': '1'});
 		o.depends({'type': 'trojan', 'tls': '1'});
 		o.depends({'type': 'vmess', 'tls': '1'});
-		o.rmempty = false;
 		o.modalonly = true;
 
 		o = s.option(form.MultiValue, 'tls_cipher_suites', _('Cipher suites'),
 			_('The elliptic curves that will be used in an ECDHE handshake, in preference order. If empty, the default will be used.'));
-		o.value('TLS_RSA_WITH_AES_128_CBC_SHA');
-		o.value('TLS_RSA_WITH_AES_256_CBC_SHA');
-		o.value('TLS_RSA_WITH_AES_128_GCM_SHA256');
-		o.value('TLS_RSA_WITH_AES_256_GCM_SHA384');
-		o.value('TLS_AES_128_GCM_SHA256');
-		o.value('TLS_AES_256_GCM_SHA384');
-		o.value('TLS_CHACHA20_POLY1305_SHA256');
-		o.value('TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA');
-		o.value('TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA');
-		o.value('TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA');
-		o.value('TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA');
-		o.value('TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256');
-		o.value('TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384');
-		o.value('TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256');
-		o.value('TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384');
-		o.value('TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256');
-		o.value('TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256');
+		for (var i in hp.tls_cipher_suites)
+			o.value(hp.tls_cipher_suites[i])
 		o.depends({'type': 'http', 'tls': '1'});
 		o.depends({'type': 'trojan', 'tls': '1'});
 		o.depends({'type': 'vmess', 'tls': '1'});
