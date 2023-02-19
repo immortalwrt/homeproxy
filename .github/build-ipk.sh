5e2a2a51d80b11e8771db20f01103813d24396fc
Tianling Shen <cnsztl@gmail.com>
2023-02-19 10:35:51 +0800

    chore(build): add firewall4 to dependencies

--- .github/build-ipk.sh
+++ .github/build-ipk.sh
@@ -34,7 +34,7 @@ echo -e "/etc/homeproxy/certs/" > "$TEMP_PKG_DIR/lib/upgrade/keep.d/$PKG_NAME"
 cat > "$TEMP_PKG_DIR/CONTROL/control" <<-EOF
 	Package: $PKG_NAME
 	Version: $PKG_VERSION
-	Depends: libc, sing-box, kmod-tun, curl
+	Depends: libc, sing-box, kmod-tun, curl, firewall4
 	Source: https://github.com/immortalwrt/homeproxy
 	SourceName: $PKG_NAME
 	Section: luci
