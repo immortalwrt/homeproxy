905677b939a406d40c3fce67ad26088d96c7d731
Tianling Shen <cnsztl@gmail.com>
2023-02-26 15:54:32 +0800

    chore(client): use section name when label is empty

--- htdocs/luci-static/resources/view/homeproxy/client.js
+++ htdocs/luci-static/resources/view/homeproxy/client.js
@@ -127,8 +127,7 @@ return view.extend({
 		var proxy_nodes = {};
 		uci.sections(data[0], 'node', (res) => {
 			proxy_nodes[res['.name']] =
-				String.format('[%s] %s', res.type, res.label || (stubValidator.apply('ip6addr', res.address || '') ?
-					String.format('[%s]', res.address) : res.address) + ':' + res.port);
+				String.format('[%s] %s', res.type, res.label || res['.name']);
 		});
 
 		s = m.section(form.NamedSection, 'config', 'homeproxy');
