0af7ebb442fc589395abaf2629123410f839d0bd
Tianling Shen <cnsztl@gmail.com>
2024-01-24 12:54:38 +0800

    feat(node): parse host param even if tls is enabled
    we don't give a shit about those goddamn proxy providers

--- htdocs/luci-static/resources/view/homeproxy/node.js
+++ htdocs/luci-static/resources/view/homeproxy/node.js
@@ -195,9 +195,7 @@ function parseShareLink(uri, features) {
 				config.grpc_servicename = params.get('serviceName');
 				break;
 			case 'ws':
-				/* We don't parse "host" param when TLS is enabled, as some providers are abusing it (host vs sni)
-				 * config.ws_host = params.get('host') ? decodeURIComponent(params.get('host')) : null;
-				 */
+				config.ws_host = params.get('host') ? decodeURIComponent(params.get('host')) : null;
 				config.ws_path = params.get('path') ? decodeURIComponent(params.get('path')) : null;
 				if (config.ws_path && config.ws_path.includes('?ed=')) {
 					config.websocket_early_data_header = 'Sec-WebSocket-Protocol';
@@ -274,8 +272,7 @@ function parseShareLink(uri, features) {
 				}
 				break;
 			case 'ws':
-				/* We don't parse "host" param when TLS is enabled, as some providers are abusing it (host vs sni) */
-				config.ws_host = (config.tls !== '1' && params.get('host')) ? decodeURIComponent(params.get('host')) : null;
+				config.ws_host = params.get('host') ? decodeURIComponent(params.get('host')) : null;
 				config.ws_path = params.get('path') ? decodeURIComponent(params.get('path')) : null;
 				if (config.ws_path && config.ws_path.includes('?ed=')) {
 					config.websocket_early_data_header = 'Sec-WebSocket-Protocol';
@@ -332,8 +329,7 @@ function parseShareLink(uri, features) {
 				}
 				break;
 			case 'ws':
-				/* We don't parse "host" param when TLS is enabled, as some providers are abusing it (host vs sni) */
-				config.ws_host = (config.tls !== '1') ? uri.host : null;
+				config.ws_host = uri.host;
 				config.ws_path = uri.path;
 				if (config.ws_path && config.ws_path.includes('?ed=')) {
 					config.websocket_early_data_header = 'Sec-WebSocket-Protocol';
