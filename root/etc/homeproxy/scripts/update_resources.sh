e2947908a1be75aa4ffc257c616d279e36a54f59
HenTele <160483143+HenTele@users.noreply.github.com>
2024-11-14 18:37:39 +0800

    discard lock file

--- root/etc/homeproxy/scripts/update_resources.sh
+++ root/etc/homeproxy/scripts/update_resources.sh
@@ -16,23 +16,6 @@ log() {
 	echo -e "$(date "+%Y-%m-%d %H:%M:%S") $*" >> "$LOG_PATH"
 }
 
-set_lock() {
-	local act="$1"
-	local type="$2"
-
-	local lock="$RUN_DIR/update_resources-$type.lock"
-	if [ "$act" = "set" ]; then
-		if [ -e "$lock" ]; then
-			log "[$(to_upper "$type")] A task is already running."
-			exit 2
-		else
-			touch "$lock"
-		fi
-	elif [ "$act" = "remove" ]; then
-		rm -f "$lock"
-	fi
-}
-
 to_upper() {
 	echo -e "$1" | tr "[a-z]" "[A-Z]"
 }
@@ -44,15 +27,12 @@ check_list_update() {
 	local listname="$4"
 	local wget="wget --timeout=10 -q"
 
-	set_lock "set" "$listtype"
-
 	local list_info="$($wget -O- "https://api.github.com/repos/$listrepo/commits?sha=$listref&path=$listname")"
 	local list_sha="$(echo -e "$list_info" | jsonfilter -e "@[0].sha")"
 	local list_ver="$(echo -e "$list_info" | jsonfilter -e "@[0].commit.message" | grep -Eo "[0-9-]+" | tr -d '-')"
 	if [ -z "$list_sha" ] || [ -z "$list_ver" ]; then
 		log "[$(to_upper "$listtype")] Failed to get the latest version, please retry later."
 
-		set_lock "remove" "$listtype"
 		return 1
 	fi
 
@@ -61,25 +41,24 @@ check_list_update() {
 		log "[$(to_upper "$listtype")] Current version: $list_ver."
 		log "[$(to_upper "$listtype")] You're already at the latest version."
 
-		set_lock "remove" "$listtype"
 		return 3
 	else
 		log "[$(to_upper "$listtype")] Local version: $local_list_ver, latest version: $list_ver."
 	fi
 
-	$wget "https://fastly.jsdelivr.net/gh/$listrepo@$list_sha/$listname" -O "$RUN_DIR/$listname"
-	if [ $? -eq 0 ] && [ -s "$RUN_DIR/$listname" ]; then
- 		mv -f "$RUN_DIR/$listname" "$RESOURCES_DIR/$listtype.${listname##*.}"
+	_TEMP_FILE="$RUN_DIR/._$(date '+%Y-%m-%d-%H-%M')-$(cat /proc/sys/kernel/random/uuid)_$listname"
+	$wget "https://fastly.jsdelivr.net/gh/$listrepo@$list_sha/$listname" -O "$_TEMP_FILE"
+	if [ $? -eq 0 ] && [ -s "$_TEMP_FILE" ]; then
+ 		mv -f "$_TEMP_FILE" "$RESOURCES_DIR/$listtype.${listname##*.}"
 		echo -e "$list_ver" > "$RESOURCES_DIR/$listtype.ver"
 		log "[$(to_upper "$listtype")] Successfully updated."
 		return 0
   	else
-		rm -f "$RUN_DIR/$listname"
+		rm -f "$_TEMP_FILE"
 		log "[$(to_upper "$listtype")] Update failed."
 		return 1
 	fi
  
-	set_lock "remove" "$listtype"	
 }
 
 case "$1" in
