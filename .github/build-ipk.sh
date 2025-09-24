7fad9945287019e698662e320220955b23d71bf3
Tianling Shen <cnsztl@immortalwrt.org>
2025-09-24 21:30:23 +0800

    feat(CI): add apk build
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- .github/build-ipk.sh
+++ .github/build-ipk.sh
@@ -6,6 +6,9 @@
 set -o errexit
 set -o pipefail
 
+PKG_MGR="${1:-apk}"
+RELEASE_TYPE="${2:-snapshot}"
+
 export PKG_SOURCE_DATE_EPOCH="$(date "+%s")"
 
 BASE_DIR="$(cd "$(dirname $0)"; pwd)"
@@ -19,20 +22,23 @@ PKG_NAME="$(get_mk_value "PKG_NAME")"
 if [ "$RELEASE_TYPE" == "release" ]; then
 	PKG_VERSION="$(get_mk_value "PKG_VERSION")"
 else
-	PKG_VERSION="dev-$PKG_SOURCE_DATE_EPOCH-$(git rev-parse --short HEAD)"
+	PKG_VERSION="$PKG_SOURCE_DATE_EPOCH~$(git rev-parse --short HEAD)"
 fi
 
 TEMP_DIR="$(mktemp -d -p $BASE_DIR)"
 TEMP_PKG_DIR="$TEMP_DIR/$PKG_NAME"
-mkdir -p "$TEMP_PKG_DIR/CONTROL/"
 mkdir -p "$TEMP_PKG_DIR/lib/upgrade/keep.d/"
 mkdir -p "$TEMP_PKG_DIR/usr/lib/lua/luci/i18n/"
 mkdir -p "$TEMP_PKG_DIR/www/"
+if [ "$PKG_MGR" == "apk" ]; then
+	mkdir -p "$TEMP_PKG_DIR/lib/apk/packages/"
+else
+	mkdir -p "$TEMP_PKG_DIR/CONTROL/"
+fi
 
 cp -fpR "$PKG_DIR/htdocs"/* "$TEMP_PKG_DIR/www/"
 cp -fpR "$PKG_DIR/root"/* "$TEMP_PKG_DIR/"
 
-echo -e "/etc/config/homeproxy" > "$TEMP_PKG_DIR/CONTROL/conffiles"
 cat > "$TEMP_PKG_DIR/lib/upgrade/keep.d/$PKG_NAME" <<-EOF
 /etc/homeproxy/certs/
 /etc/homeproxy/ruleset/
@@ -40,55 +46,114 @@ cat > "$TEMP_PKG_DIR/lib/upgrade/keep.d/$PKG_NAME" <<-EOF
 /etc/homeproxy/resources/proxy_list.txt
 EOF
 
-cat > "$TEMP_PKG_DIR/CONTROL/control" <<-EOF
-	Package: $PKG_NAME
-	Version: $PKG_VERSION
-	Depends: libc, sing-box, firewall4, kmod-nft-tproxy, ucode-mod-digest
-	Source: https://github.com/immortalwrt/homeproxy
-	SourceName: $PKG_NAME
-	Section: luci
-	SourceDateEpoch: $PKG_SOURCE_DATE_EPOCH
-	Maintainer: Tianling Shen <cnsztl@immortalwrt.org>
-	Architecture: all
-	Installed-Size: TO-BE-FILLED-BY-IPKG-BUILD
-	Description:  The modern ImmortalWrt proxy platform for ARM64/AMD64
-EOF
+po2lmo "$PKG_DIR/po/zh_Hans/homeproxy.po" "$TEMP_PKG_DIR/usr/lib/lua/luci/i18n/homeproxy.zh-cn.lmo"
 
-git clone --filter=blob:none --no-checkout "https://github.com/openwrt/luci.git" "po2lmo"
-pushd "po2lmo"
-git config core.sparseCheckout true
-echo "modules/luci-base/src" >> ".git/info/sparse-checkout"
-git checkout
-cd "modules/luci-base/src"
-make po2lmo
-./po2lmo "$PKG_DIR/po/zh_Hans/homeproxy.po" "$TEMP_PKG_DIR/usr/lib/lua/luci/i18n/homeproxy.zh-cn.lmo"
-popd
-rm -rf "po2lmo"
+if [ "$PKG_MGR" == "apk" ]; then
+	find "$TEMP_PKG_DIR" -type f,l -printf '/%P\n' | sort > "$TEMP_PKG_DIR/lib/apk/packages/$PKG_NAME.list"
+	echo "/etc/config/homeproxy" >> "$TEMP_PKG_DIR/lib/apk/packages/$PKG_NAME.conffiles"
+	cat "$TEMP_PKG_DIR/lib/apk/packages/$PKG_NAME.conffiles" | while IFS= read -r file; do
+		[ -f "$TEMP_PKG_DIR/$file" ] || continue
+		sha256sum "$TEMP_PKG_DIR/$file" | sed "s,$TEMP_PKG_DIR/,," >> "$TEMP_PKG_DIR/lib/apk/packages/$PKG_NAME.conffiles_static"
+	done
 
-echo -e '#!/bin/sh
+	echo -e '#!/bin/sh
+[ "${IPKG_NO_SCRIPT}" = "1" ] && exit 0
+[ -s ${IPKG_INSTROOT}/lib/functions.sh ] || exit 0
+. ${IPKG_INSTROOT}/lib/functions.sh
+export root="${IPKG_INSTROOT}"
+export pkgname="'"$PKG_NAME"'"
+add_group_and_user
+default_postinst
+[ -n "${IPKG_INSTROOT}" ] || { rm -f /tmp/luci-indexcache.*
+	rm -rf /tmp/luci-modulecache/
+	killall -HUP rpcd 2>/dev/null
+	exit 0
+}' > "$TEMP_DIR/post-install"
+
+	echo -e '#!/bin/sh
+export PKG_UPGRADE=1
+#!/bin/sh
+[ "${IPKG_NO_SCRIPT}" = "1" ] && exit 0
+[ -s ${IPKG_INSTROOT}/lib/functions.sh ] || exit 0
+. ${IPKG_INSTROOT}/lib/functions.sh
+export root="${IPKG_INSTROOT}"
+export pkgname="'"$PKG_NAME"'"
+add_group_and_user
+default_postinst
+[ -n "${IPKG_INSTROOT}" ] || { rm -f /tmp/luci-indexcache.*
+	rm -rf /tmp/luci-modulecache/
+	killall -HUP rpcd 2>/dev/null
+	exit 0
+}' > "$TEMP_DIR/post-upgrade"
+
+	echo -e '#!/bin/sh
+[ -s ${IPKG_INSTROOT}/lib/functions.sh ] || exit 0
+. ${IPKG_INSTROOT}/lib/functions.sh
+export root="${IPKG_INSTROOT}"
+export pkgname="'"$PKG_NAME"'"
+default_prerm' > "$TEMP_DIR/pre-deinstall"
+
+	apk mkpkg \
+		--info "name:$PKG_NAME" \
+		--info "version:$PKG_VERSION" \
+		--info "description:The modern ImmortalWrt proxy platform for ARM64/AMD64" \
+		--info "arch:all" \
+		--info "origin:https://github.com/immortalwrt/homeproxy" \
+		--info "url:" \
+		--info "maintainer:Tianling Shen <cnsztl@immortalwrt.org>" \
+		--info "provides:" \
+		--script "post-install:$TEMP_DIR/post-install" \
+		--script "post-upgrade:$TEMP_DIR/post-upgrade" \
+		--script "pre-deinstall:$TEMP_DIR/pre-deinstall" \
+		--info "depends:libc sing-box firewall4 kmod-nft-tproxy ucode-mod-digest" \
+		--files "$TEMP_PKG_DIR" \
+		--output "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}.apk"
+
+	mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}.apk" "$BASE_DIR/${PKG_NAME}_${PKG_VERSION}_all.apk"
+else
+	mkdir -p "$TEMP_PKG_DIR/CONTROL/"
+
+	cat > "$TEMP_PKG_DIR/CONTROL/control" <<-EOF
+		Package: $PKG_NAME
+		Version: $PKG_VERSION
+		Depends: libc, sing-box, firewall4, kmod-nft-tproxy, ucode-mod-digest
+		Source: https://github.com/immortalwrt/homeproxy
+		SourceName: $PKG_NAME
+		Section: luci
+		SourceDateEpoch: $PKG_SOURCE_DATE_EPOCH
+		Maintainer: Tianling Shen <cnsztl@immortalwrt.org>
+		Architecture: all
+		Installed-Size: TO-BE-FILLED-BY-IPKG-BUILD
+		Description:  The modern ImmortalWrt proxy platform for ARM64/AMD64
+	EOF
+	chmod 0644 "$TEMP_PKG_DIR/CONTROL/control"
+
+	echo -e "/etc/config/homeproxy" > "$TEMP_PKG_DIR/CONTROL/conffiles"
+
+	echo -e '#!/bin/sh
 [ "${IPKG_NO_SCRIPT}" = "1" ] && exit 0
 [ -s ${IPKG_INSTROOT}/lib/functions.sh ] || exit 0
 . ${IPKG_INSTROOT}/lib/functions.sh
 default_postinst $0 $@' > "$TEMP_PKG_DIR/CONTROL/postinst"
-chmod 0755 "$TEMP_PKG_DIR/CONTROL/postinst"
+	chmod 0755 "$TEMP_PKG_DIR/CONTROL/postinst"
 
-echo -e "[ -n "\${IPKG_INSTROOT}" ] || {
+	echo -e "[ -n "\${IPKG_INSTROOT}" ] || {
 	(. /etc/uci-defaults/$PKG_NAME) && rm -f /etc/uci-defaults/$PKG_NAME
 	rm -f /tmp/luci-indexcache
 	rm -rf /tmp/luci-modulecache/
 	exit 0
 }" > "$TEMP_PKG_DIR/CONTROL/postinst-pkg"
-chmod 0755 "$TEMP_PKG_DIR/CONTROL/postinst-pkg"
+	chmod 0755 "$TEMP_PKG_DIR/CONTROL/postinst-pkg"
 
-echo -e '#!/bin/sh
+	echo -e '#!/bin/sh
 [ -s ${IPKG_INSTROOT}/lib/functions.sh ] || exit 0
 . ${IPKG_INSTROOT}/lib/functions.sh
 default_prerm $0 $@' > "$TEMP_PKG_DIR/CONTROL/prerm"
-chmod 0755 "$TEMP_PKG_DIR/CONTROL/prerm"
+	chmod 0755 "$TEMP_PKG_DIR/CONTROL/prerm"
 
-curl -fsSL "https://raw.githubusercontent.com/openwrt/openwrt/master/scripts/ipkg-build" -o "$TEMP_DIR/ipkg-build"
-chmod 0755 "$TEMP_DIR/ipkg-build"
-"$TEMP_DIR/ipkg-build" -m "" "$TEMP_PKG_DIR" "$TEMP_DIR"
+	ipkg-build -m "" "$TEMP_PKG_DIR" "$TEMP_DIR"
 
-mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk"
+	mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk"
+fi
+
 rm -rf "$TEMP_DIR"
